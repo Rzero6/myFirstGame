dc456d10e669fc08dae75d03348591eb6464110f
Reynold <michaelreynoldk@gmail.com>
2024-04-23 02:29:11 +0700

    add main menu

---
+++ Assets/Script_Game/mainMenu.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class mainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
